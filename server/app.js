8c4bd6a7a6d1e165904b6580b3033dfdfd3c674c
MarkDrastrup <mark.drastrup@gmail.com>
2019-08-12 20:41:03 +0900

    adding test route for Heroku

--- server/app.js
+++ server/app.js
@@ -4,7 +4,11 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my place");
+  res.send("Welcome to Leftovers");
+});
+
+app.get("/food", (req, res) => {
+  res.send("Here's a list of fooditems");
 });
 
 app.listen(PORT, () => {
