88c87e3ae111e2693589cb4d20d5072a290114ea
MarkDrastrup <mark.drastrup@gmail.com>
2019-08-14 11:22:55 +0900

    Adding catch all route

--- server/app.js
+++ server/app.js
@@ -171,6 +171,14 @@ app.post("/api/recipes", async (req, res) => {
   res.send(recipes);
 });
 
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "path/to/your/index.html"), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`App is running on ${PORT}`);
 });
