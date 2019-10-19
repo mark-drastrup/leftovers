b7ddecf6e1471bfc183202e6b8c9fa01c4d91be3
mark-drastrup <mark.drastrup@gmail.com>
2019-10-19 17:44:31 +0900

    Refactoring project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import Recipes from "./components/Recipes"
-import SearchBar from "./components/SearchBar"
-import { Container, Row, Col } from 'react-bootstrap';
-import './App.css';
-import './index.css';
+import React from "react";
+import Recipes from "./components/Recipes";
+import SearchBar from "./components/SearchBar";
+import Navbar from "./components/Navbar";
+import { Container, Row, Col } from "react-bootstrap";
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
@@ -11,20 +12,20 @@ function App() {
       <Container className="hero" fluid="true">
         <Row className="hero__row">
           <Col md={12} className="hero__sidebar">
-            <div className="hero__menu">
-              <p>Sign Up</p>
-              <p>Sign In</p>
-            </div>
+            <Navbar />
             <div className="hero__searchbar">
-              <h1>
-                Welcome to Leftovers
-            </h1>
-              <h3>Leftovers prevents food waste by providing recipes with food you have in your fridge</h3>
+              <h1>Welcome to Leftovers</h1>
+              <h3>
+                Leftovers prevents food waste by providing recipes with food you
+                have in your fridge
+              </h3>
               <SearchBar></SearchBar>
-              <p className="hero__helptext">Write ingredients separated by a space: "pasta tomato avocado cheese"</p>
+              <p className="hero__helptext">
+                Write ingredients separated by a space: "pasta tomato avocado
+                cheese"
+              </p>
             </div>
-            <div>
-            </div>
+            <div></div>
           </Col>
         </Row>
       </Container>
