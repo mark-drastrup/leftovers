f7732c60ef7eec86988424a23426c2721b6697dc
mark-drastrup <mark.drastrup@gmail.com>
2019-10-19 17:30:36 +0900

    Moving Redux store to own file

--- src/index.js
+++ src/index.js
@@ -3,100 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import FoodList from "./components/FoodList";
-import AuthForm from "./components/AuthForm";
 import * as serviceWorker from "./serviceWorker";
+import store from "./store";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-
-const fetchRecipes = data => {
-  const newAction = {
-    type: "FETCH_RECIPES",
-    data: data
-  };
-  return newAction;
-};
-
-const fetchFood = data => {
-  const newAction = {
-    type: "FETCH_FOOD",
-    data: data
-  };
-  return newAction;
-};
-
-const addFood = data => {
-  const newAction = {
-    type: "ADD_FOOD",
-    data: data
-  };
-  return newAction;
-};
-
-const deleteFood = data => {
-  const newAction = {
-    type: "DELETE_FOOD",
-    data: data
-  };
-  return newAction;
-};
-
-const updateQuery = data => {
-  const newAction = {
-    type: "UPDATE_QUERY",
-    data: data
-  };
-  return newAction;
-};
-
-const initialState = {
-  recipes: [],
-  query: "",
-  food: []
-};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "FETCH_RECIPES": {
-      const copiedState = Object.assign({}, state);
-      copiedState.recipes = [...action.data];
-      return copiedState;
-    }
-    case "UPDATE_QUERY": {
-      const copiedState = Object.assign({}, state);
-      copiedState.query = action.data;
-      return copiedState;
-    }
-    case "FETCH_FOOD": {
-      const copiedState = Object.assign({}, state);
-      copiedState.food = [...copiedState.food, ...action.data];
-      return copiedState;
-    }
-    case "ADD_FOOD": {
-      const copiedState = Object.assign({}, state);
-      copiedState.food = [...copiedState.food, action.data.data[0]];
-      console.log(copiedState);
-      return copiedState;
-    }
-    case "DELETE_FOOD": {
-      const copiedState = Object.assign({}, state);
-      console.log(action.data.data[0]);
-      copiedState.food = copiedState.food.filter(
-        item => item.id !== action.data.data[0].id
-      );
-      return copiedState;
-    }
-    default: {
-      return state;
-    }
-  }
-};
-
-const store = createStore(
-  reducer,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 const Root = ({ store }) => (
   <Provider store={store}>
@@ -105,7 +15,6 @@ const Root = ({ store }) => (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/user/food" component={FoodList} />
-        <Route path="/login" component={AuthForm} />
       </Switch>
     </Router>
   </Provider>
